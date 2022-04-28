24eefc82916e91bf3e0a90477b7e3bd278d0150e
Clbl <hahakim77@naver.com>
2022-04-28 10:42:19 +0900

    error solved

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 
+@api_view(['POST'])
+def get_create_ad(request):
+    if request.method == 'GET':
+        pass    
+
 
 @api_view(['PATCH', 'DELETE'])
 def update_delete_ad(request):
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ads import views
 
 urlpatterns = [
     path('admin', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
+    path('advertise',views.post_create_ad)
 ]
