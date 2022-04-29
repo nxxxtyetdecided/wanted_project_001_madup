655af055f85f5192c99268d24eb3676a1dc37e57
Clbl <hahakim77@naver.com>
2022-04-29 16:14:52 +0900

    Feat: add create test case code
    Fix: Change column name in Ad table and Result table

--- ads/models.py
+++ ads/models.py
@@ -4,8 +4,7 @@ from core.models import TimeStampModel
    
 
 class Ad(TimeStampModel):
-    user = models.ForeignKey('users.User', db_index=True, on_delete=models.CASCADE)  # 광고주 id    
-    #uid = models.AutoField( db_index=True, primary_key=True)  # 해당 광고 고유 번호 , 자동 증가추가
+    advertiser = models.ForeignKey('users.User', db_index=True, on_delete=models.CASCADE)  # 광고주 id    
     uid = models.CharField(max_length=50, db_index=True, primary_key=True)  # 해당 광고 고유 번호
     start_date = models.DateField(null=True, blank=True)  # 광고 시작 날짜
     end_date = models.DateField(null=True, blank=True)  # 광고 종료 날짜
@@ -19,7 +18,7 @@ class Ad(TimeStampModel):
 
 
 class Result(TimeStampModel):
-    ad = models.ForeignKey('Ad', on_delete=models.CASCADE, db_index=True)
+    uid = models.ForeignKey('Ad', on_delete=models.CASCADE, db_index=True)
     media = models.CharField(max_length=15, db_index=True)
     date = models.DateField(db_index=True)
     cost = models.PositiveIntegerField(default=0)
--- ads/views.py
+++ ads/views.py
@@ -74,11 +74,29 @@ def get_create_ad(request):
     # 필수 입력값 4개(ad) + 1개 (result) 입력시 ad를 하나(캠페인)생성 , 기간에 따라 하루마다 하나씩 result를 생성
     if request.method == 'POST':
 
+<<<<<<< HEAD
+<<<<<<< HEAD
         start_date = request.data['start_date']
         end_date   = request.data['end_date']
+        
         user_id    = request.data['user_id']
         media      = request.data['media']
         uid        = request.data['uid']       
+=======
+=======
+>>>>>>> 1c7743c (Fix: Change column name in Ad table and Result table)
+        start_date    = request.data['start_date']
+        end_date      = request.data['end_date']
+        advertiser_id = request.data['advertiser_id']
+        media         = request.data['media']
+        uid           = request.data['uid']       
+<<<<<<< HEAD
+        
+        if not start_date or not end_date or not advertiser_id or not media or not uid:
+            return Response({'MESSAGE': 'MISSING_VALUE'}, status = 400)
+>>>>>>> 8b5f382 (Feat: add create test case code)
+=======
+>>>>>>> 1c7743c (Fix: Change column name in Ad table and Result table)
         
         #end-start day로 차이나는 값 만큼 result를 생성 (최소1)
         start = datetime.strptime(start_date, '%Y-%m-%d')
@@ -93,15 +111,15 @@ def get_create_ad(request):
 
         
         new_ad = Ad.objects.create(
-            start_date = start_date,
-            end_date   = end_date,
-            user_id    = user_id,
-            uid        = uid
+            start_date    = start_date,
+            end_date      = end_date,
+            advertiser_id = advertiser_id,
+            uid           = uid
         )
         
         #필수 데이터 외에 추가데이터
         if 'budget' in request.data:
-            new_ad.budget          = request.data['budget']
+            new_ad.budget = request.data['budget']
             if float(request.data['budget']) < 0:
                 return Response({'MESSAGE': 'INVALID_VALUE'}, status = 400)
             
@@ -113,17 +131,26 @@ def get_create_ad(request):
         for day in range(day.days+1):
             date = start + timedelta(days=day)
             Result.objects.create(
+<<<<<<< HEAD
+<<<<<<< HEAD
                 ad=new_ad,
                 media=media,
                 date=date
+=======
+                uid   = new_ad,
+                media = media,
+                date  = date
+>>>>>>> 8b5f382 (Feat: add create test case code)
+=======
+                ad = new_ad,
+                media = media,
+                date = date
+>>>>>>> 1c7743c (Fix: Change column name in Ad table and Result table)
             )
 
         serializer = AdSerializer(new_ad)
         return Response(serializer.data)
-    
-    #POST가 아닐때
-    else :
-        return Response({'MESSAGE': 'Method Not Allowed'}, status = 405)
+   
         
 
 
@@ -138,17 +165,16 @@ def update_delete_ad(request, advertiser, uid):
         """
         try:
             data = json.loads(request.body)
-            ad = Ad.objects.get(user = advertiser, uid = uid)
-
-            start_date = datetime.strptime(str(data['start_date']), '%Y-%m-%d').date(),
-            end_date = datetime.strptime(str(data['end_date']), '%Y-%m-%d').date(),
-            budget = data['budget']
-            estimated_spend = data['estimated_spend']
+            ad = Ad.objects.get(advertiser = advertiser, uid = uid)
+            start_date = data.get('start_date', ad.start_date)
+            end_date = data.get('end_date', ad.end_date)
+            budget = data.get('budget', ad.budget)
+            estimated_spend = data.get('estimated_spend', ad.estimated_spend)
 
             if start_date <= datetime.now().date():
                 return JsonResponse({'MESSAGE': 'INVALID_DATE'}, status = 400)
 
-            if start_date >= end_date:
+            if start_date > end_date:
                 return JsonResponse({'MESSAGE': 'INVALID_DATE'}, status = 400)
 
             if budget < 0 or estimated_spend < 0:
@@ -170,15 +196,16 @@ def update_delete_ad(request, advertiser, uid):
 
     elif request.method == 'DELETE':
         """
-                    정미정 (soft delete로 구현)
-                """
+            정미정 (soft delete로 구현)
+        """
         try:
             ad = Ad.objects.get(uid=uid)
             serializer = AdSerializer(ad)
             ad.delete_at = datetime.now()
             ad.is_delete = True
             ad.save()
-            return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status=status.HTTP_201_CREATED, data=serializer.data)
+
         except Ad.DoesNotExist:
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'MESSAGE': 'AD_DOES_NOT_EXIST'}, status=status.HTTP_404_NOT_FOUND)
 
